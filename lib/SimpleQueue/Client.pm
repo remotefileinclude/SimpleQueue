1c17fc434ab599cddf53bad09025d39205dde124
remotefileinclude <rfi@remotefileinclude.net>
2012-11-24 00:02:35 -0800

    convert to logging in Client.pm

--- lib/SimpleQueue/Client.pm
+++ lib/SimpleQueue/Client.pm
@@ -6,7 +6,7 @@ use warnings;
 use IO::Socket;
 use IO::Select;
 use Socket;
-use Log::Log4perl;
+use Log::Log4perl qw| :easy |;
 use Carp;
 use SimpleQueue::Common qw|sq_decode|;
 
@@ -69,17 +69,17 @@ sub _run_main_loop {
     my ( $self ) = @_;
 
     while (1) {
-        print "Checking for messages\n";
+        TRACE("Checking for messages");
         my ($incoming,$outgoing) = 
             IO::Select->select( $self->{select_loop}, $self->{select_loop}, undef, 1);
 
         foreach my $server ( @{$incoming} ) {
-	        print "Got message\n";
+	        TRACE("Got message");
             
             my $message = sq_decode($server) ; 
 
             if (!$message) {
-                print "Lost server\n";
+                ERROR("Lost server");
                 $self->{select_loop}->remove($server);
                 exit;
             } 
@@ -90,13 +90,13 @@ sub _run_main_loop {
         foreach my $server ( @{$outgoing} ) {
 
             if ( $self->{since_ping}->{"$server"}++ > $PING_INT ) {
-                print "ping\n";
+                TRACE("ping");
                 print $server "ping";
 	            eval {
                     local $SIG{ALRM}  = sub { die "ping timeout"};
                     alarm $self->{timeout};
                     sysread( $server, my $pong, 4 );
-	    	        die "no pong" unless ( $pong && ($pong eq 'pong'));
+	    	        croak "no pong" unless ( $pong && ($pong eq 'pong'));
 
 	    	        print "$pong\n";
                     alarm 0;
@@ -104,7 +104,7 @@ sub _run_main_loop {
                 };
                 if ($@) {
                     $self->{select_loop}->remove($server);
-                    print "Server error $@\n";
+                    ERROR("Server error $@");
                     exit;
 	            }
             }
